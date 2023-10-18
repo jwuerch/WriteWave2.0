a0be04e26d57e0e431008517a29f0b3fb9d74641
J <J@gmail.com>
2023-10-18 17:18:24 +0200

    make copy_serp_data.py faster

--- copy_serp_data.py
+++ copy_serp_data.py
@@ -2,7 +2,6 @@ import gspread
 import os
 from google.oauth2.service_account import Credentials
 from dotenv import load_dotenv
-import time
 
 # Load the .env file
 load_dotenv()
@@ -31,6 +30,13 @@ datasheet_col_index = header_row.index('Google Sheet')
 start_range -= 1
 end_range -= 1
 
+def get_column_letter(n):
+    string = ""
+    while n > 0:
+        n, remainder = divmod(n - 1, 26)
+        string = chr(65 + remainder) + string
+    return string
+
 print(f'>>> START copy_keyword_variations.py <<<\n')
 # Process only rows within the defined range
 for i, row in enumerate(all_values[start_range:end_range + 1], start=start_range):
@@ -49,26 +55,22 @@ for i, row in enumerate(all_values[start_range:end_range + 1], start=start_range
     serp_worksheet = datasheet.worksheet('SERP Data')
     keyword_variations_worksheet = datasheet.worksheet('Keyword Variations')
 
-    # Get the index of 'Keyword Variations' and 'Search Result' columns
+    # Get the index of 'Keyword Variations' column
     serp_header_row = serp_worksheet.row_values(2)
     keyword_variations_col_index = serp_header_row.index('Keyword Variations')
-    search_result_col_index = serp_header_row.index('Search Result')
 
-    # Get all the keyword variations and search results
+    # Get all the keyword variations
     keyword_variations = serp_worksheet.col_values(keyword_variations_col_index + 1)[2:]  # Skip the header row and the empty row
-    search_results = serp_worksheet.col_values(search_result_col_index + 1)[2:]  # Skip the header row and the empty row
 
-    # Paste the keyword variations and search results into the 'Keyword Variations' worksheet
-    for j, variation in enumerate(keyword_variations, start=1):
-        keyword_variations_worksheet.update_cell(3, j + 3, variation)  # Start from column D (index 4)
+    # Prepare the data for the update
+    # This will create a list of lists where each inner list is a row
+    keyword_variations_data = [keyword_variations]
 
-    for j, result in enumerate(search_results, start=1):
-        keyword_variations_worksheet.update_cell(j + 3, 2, result)  # Start from row B2
+    # Calculate the end column letter based on the number of keyword variations
+    end_column_letter = get_column_letter(len(keyword_variations) + 3)  # +3 because we start from column D (index 4)
 
-    # Add a pause every 30 variations
-    if j % 30 == 0:
-        print(f"Copied {j} keyword variations. Pausing for 60 seconds...")
-        time.sleep(60)
+    # Update the cells in the 'Keyword Variations' worksheet starting from D2
+    keyword_variations_worksheet.update(f'D2:{end_column_letter}2', keyword_variations_data)
 
     print(f"Copied all keyword variations for row {i + 1}")
 
