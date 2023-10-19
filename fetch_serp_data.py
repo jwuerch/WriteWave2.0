b3313c00e00f9c9859e1f98401b8b0f5474fb23f
J <J@gmail.com>
2023-10-19 16:47:07 +0200

    optimize fetch_serp_data.py and make sure it reiterates over the 5 searches to get necessary factors

--- fetch_serp_data.py
+++ fetch_serp_data.py
@@ -1,9 +1,7 @@
 from serpapi import GoogleSearch
 import time
-from collections import defaultdict
 import random
-import time
-
+from collections import defaultdict
 
 def fetch_serp_data(client, keyword_sheet_url, keyword, serpapi_api_key):
 
@@ -14,17 +12,16 @@ def fetch_serp_data(client, keyword_sheet_url, keyword, serpapi_api_key):
     # Open the 'SERP Data' worksheet
     serp_worksheet = datasheet.worksheet('SERP Data')
 
-    # Initialize a dictionary to store the rankings
+    # Get the index of 'Search Result' column
+    serp_header_row = serp_worksheet.row_values(2)
+    search_result_col_index = serp_header_row.index('Search Result')
+
+    # Initialize a dictionary to store the rankings and related questions
     rankings = defaultdict(list)
-
-    # Initialize a set to store the unique keyword variations
+    related_questions = {}
+    video_links = {}
     keyword_variations = set()
-
-    # Initialize variables to store the results
-    pas_questions = []
-    pas_answers = []
     featured_snippet = None
-    video_urls = []
 
     # Perform 5 searches and calculate the average ranking
     for _ in range(5):
@@ -43,38 +40,43 @@ def fetch_serp_data(client, keyword_sheet_url, keyword, serpapi_api_key):
         # Check if 'organic_results' is in the results
         if 'organic_results' in results:
             # Extract the URLs and their rankings
-            for rank, result in enumerate(results['organic_results'][:100], start=1):
-                # Add the 'snippet_highlighted_words' to the keyword variations set
+            for rank, result in enumerate(results['organic_results'], start=1):
+                url = result['link']
+                rankings[url].append(rank)
+                # Check if 'snippet_highlighted_words' is in the result
                 if 'snippet_highlighted_words' in result:
+                    # Add the keyword variations to the set
                     keyword_variations.update(result['snippet_highlighted_words'])
 
-                # Only process the other data for the top 10 results
-                if rank <= 10:
-                    url = result['link']
-                    rankings[url].append(rank)
+                # print(f"Rank: {rank}, URL: {url}")  # Print to console
 
-        # Check if 'people_also_ask' is in the results
-        if 'related_questions' in results and not pas_questions:
-            # Extract the People Also Ask questions and answers
-            pas_questions = [pas['question'] for pas in results['related_questions']]
-            pas_answers = [pas.get('snippet') for pas in results['related_questions']]  # Use get method here
-
-        # Check if 'answer_box' is in the results
-        if 'answer_box' in results and not featured_snippet:
-            # Extract the answer from 'answer_box'
-            featured_snippet = results['answer_box']['snippet']
+        # Check if 'related_questions' is in the results
+        if 'related_questions' in results:
+            # Extract the questions and their answers
+            for question in results['related_questions']:
+                q = question['question']
+                a = question.get('snippet', '')
+                if q not in related_questions:
+                    related_questions[q] = a
 
         # Check if 'inline_videos' is in the results
-        if 'inline_videos' in results and not video_urls:
-            # Extract the URLs of the top 10 video results
-            video_urls = [video['link'] for video in results['inline_videos'][:10]]
+        if 'inline_videos' in results:
+            # Extract the video links
+            for video in results['inline_videos']:
+                link = video['link']
+                if link not in video_links:
+                    video_links[link] = True
+
+        # Check if 'answer_box' is in the results and featured_snippet is not yet set
+        if 'answer_box' in results and not featured_snippet:
+            # Extract the featured snippet
+            featured_snippet = results['answer_box']['snippet']
 
         # Add a 30-second delay between each search
         print(f'Successfully completed a search.')
-
-        # Generate a random number between 5 and 10
-        sleep_time = random.uniform(5, 7)
-        time.sleep(sleep_time)
+        random_number = random.uniform(7, 12)
+        print(f"Waiting {random_number} seconds...")
+        time.sleep(random_number)
 
     # Calculate the average ranking for each URL
     avg_rankings = {url: sum(ranks) / len(ranks) for url, ranks in rankings.items()}
@@ -87,51 +89,61 @@ def fetch_serp_data(client, keyword_sheet_url, keyword, serpapi_api_key):
         'values': [[avg_rank] for avg_rank in avg_rankings.values()]
     })
 
-    # Extract the URLs, titles, and meta descriptions of the top 10 results
-    urls = [result['link'] for result in results['organic_results'][:10]]
-    titles = [result['title'] for result in results['organic_results'][:10]]
-    descriptions = [result.get('snippet', '') for result in results['organic_results'][:10]]
+    # Extract the URLs of all results
+    urls = [result['link'] for result in results['organic_results']]
 
     updates.append({
-        'range': f'B3:B{len(urls)+2}',
+        'range': f'B3:B{len(urls) + 2}',
         'values': [[url] for url in urls]
     })
+
+    # Extract the SEO Titles and Meta Descriptions of all results
+    titles = [result['title'] for result in results['organic_results']]
+    descriptions = [result.get('snippet', '') for result in results['organic_results']]
+
+
     updates.append({
-        'range': f'C3:C{len(titles)+2}',
+        'range': f'C3:C{len(titles) + 2}',
         'values': [[title] for title in titles]
     })
     updates.append({
-        'range': f'D3:D{len(descriptions)+2}',
+        'range': f'D3:D{len(descriptions) + 2}',
         'values': [[description] for description in descriptions]
     })
 
-    if 'related_questions' in results:
-        updates.append({
-            'range': f'E3:E{len(pas_questions)+2}',
-            'values': [[question] for question in pas_questions]
-        })
-        updates.append({
-            'range': f'F3:F{len(pas_answers)+2}',
-            'values': [[answer] for answer in pas_answers]
-        })
-
-    if 'answer_box' in results:
-        updates.append({
-            'range': 'H3:H4',
-            'values': [[featured_snippet]]
-        })
-
-    if 'inline_videos' in results:
-        updates.append({
-            'range': f'G3:G{len(video_urls)+2}',
-            'values': [[url] for url in video_urls]
-        })
-    else:
-        print(f"No organic video results found for keyword, '{keyword}'")
+    # Add related questions and their answers
+    questions = list(related_questions.keys())
+    answers = list(related_questions.values())
 
     updates.append({
-        'range': f'I3:I{len(keyword_variations) + 2}',
-        'values': [[variation.lower()] for variation in keyword_variations]
+        'range': f'E3:E{len(questions) + 2}',
+        'values': [[question] for question in questions]
+    })
+    updates.append({
+        'range': f'F3:F{len(answers) + 2}',
+        'values': [[answer] for answer in answers]
+    })
+
+    # Add video links
+    links = list(video_links.keys())
+
+    updates.append({
+        'range': f'G3:G{len(links) + 2}',
+        'values': [[link] for link in links]
+    })
+
+    # Add featured snippet
+    updates.append({
+        'range': 'H3',
+        'values': [[featured_snippet]]
+    })
+
+    # Add keyword variations
+    variations = list(keyword_variations)
+
+    updates.append({
+        'range': f'I3:I{len(variations) + 2}',
+        'values': [[variation] for variation in variations]
     })
 
     # Update the worksheet with all the data at once
